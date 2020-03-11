f3b27cdd654f545b07e9304942ee19ac1211db18
Uri Juhasz <uri.juhasz@gmail.com>
2020-03-12 00:16:35 +0100

    Verify topology including duplicate edges

--- OpenGL_test_4.6_a/Geometry/Mesh.cpp
+++ OpenGL_test_4.6_a/Geometry/Mesh.cpp
@@ -124,6 +124,10 @@ void calculateTopology1(Mesh& mesh)
 	{//Step3
 		for (int fi = 0; fi < numFaces; ++fi)
 		{
+			if (fi == 45204)
+			{
+				cout << "";
+			}
 			const auto f = faces[fi].m_vis;
 			for (int fei = 0; fei < 3; ++fei)
 			{
@@ -152,6 +156,7 @@ void calculateTopology1(Mesh& mesh)
 	//Step4
 	cout << " Step4";
 	int numEdgesX = numFaces * 3;
+	vector<array<int, 2>> duplicatedEdges;
 	{ //Step4
 		for (int vi = 0; vi < numVertices; ++vi)
 		{
@@ -161,6 +166,10 @@ void calculateTopology1(Mesh& mesh)
 			for (int i = start; i < end - 1; ++i)
 			{
 				const auto eteI = edgeTable[i];
+				if (vi == 5435 && eteI.m_vi == 52695)
+				{
+					cout << "";
+				}
 				for (int j = i + 1; j < end; ++j)
 				{
 					const auto eteJ = edgeTable[j];
@@ -170,7 +179,28 @@ void calculateTopology1(Mesh& mesh)
 						{
 							const auto fiE = eteJ.m_fiC[k];
 							if (fiE != -1)
-								edgeTable[i].m_fiC[k] = fiE;
+							{
+								if (edgeTable[i].m_fiC[k] == -1)
+								{
+									edgeTable[i].m_fiC[k] = fiE;
+								}
+								else
+								{
+									const auto fiI = edgeTable[i].m_fiC[k] >> 2;
+									const auto feiI = edgeTable[i].m_fiC[k] & 3;
+									const auto fiJ = edgeTable[j].m_fiC[k] >> 2;
+									const auto feiJ = edgeTable[j].m_fiC[k] & 3;
+									const auto& fI = faces[fiI].m_vis;
+									const auto& fJ = faces[fiJ].m_vis;
+									cerr << " Warning - triangle edges "
+										<< fiI << ":" << feiI << " [" << fI[0] << " " << fI[1] << " " << fI[2] << "]"
+										<< " and "
+										<< fiJ << ":" << feiJ << " [" << fJ[0] << " " << fJ[1] << " " << fJ[2] << "]"
+										<< " overlap on edge " << (k ? vi : eteJ.m_vi) << " - " << (k ? eteJ.m_vi : vi)
+										<< endl;
+									duplicatedEdges.emplace_back(array<int, 2>{edgeTable[i].m_fiC[k], edgeTable[j].m_fiC[k]});
+								}
+							}
 						}
 						--end;
 						if (j < end)
@@ -258,13 +288,22 @@ void calculateTopology1(Mesh& mesh)
 				for (int k = 0; k < 2; ++k)
 				{
 					const auto fi = fis[k];
+					const auto fei = ete.m_fiC[k] & 3;
 					if (fi != -1)
 					{
-						const auto fei = ete.m_fiC[k] & 3;
 						faceEdges[fi].m_feis[fei] = ei;
 					}
 				}
 			}
+		}
+		for (const auto de : duplicatedEdges)
+		{
+			const auto fi0 = de[0] >> 2;
+			const auto fei0 = de[0] & 3;
+			const auto fi1 = de[1] >> 2;
+			const auto fei1 = de[1] & 3;
+			assert(faceEdges[fi1].m_feis[fei1] == -1);
+			faceEdges[fi1].m_feis[fei1] = faceEdges[fi0].m_feis[fei0];
 		}
 	}
 	const auto step5EndTime = chrono::high_resolution_clock::now();
@@ -283,6 +322,106 @@ void calculateTopology1(Mesh& mesh)
 void Mesh::calculateTopology()
 {
 	calculateTopology1(*this);
+	validateTopology();
 }
 
+#ifdef _DEBUG
+#define alwaysAssert(b) assert(b)
+#else
+void alwaysAssert(const bool b)
+{
+	assert(b);
+	if (!b)
+		throw new int();
+}
+#endif
 
+template<class C, class T> bool rangeContains(const C& c, const int start, const int end, const T v)
+{
+	for (int i = start; i < end; ++i)
+		if (c[i] == v)
+			return true;
+	return false;
+}
+template<class C, class T> bool contains(const C& c, const T v)
+{
+	return rangeContains(c, 0, c.size(), v);
+}
+void Mesh::validateTopology()
+{
+	const auto& faces = m_faces;
+	const auto& edges = m_edges;
+	const auto F = numFaces();
+	const auto V = numVertices();
+	const int E = toInt(edges.size());
+
+	for (int fi = 0; fi < F; ++fi)
+	{
+		const auto& fvis = m_faces[fi].m_vis;
+		const auto& feis = m_faceEdges[fi].m_feis;
+		for (int fei = 0; fei < 3; ++fei)
+		{
+			const auto ei = feis[fei];
+			alwaysAssert(0 <= ei && ei < E);
+			const auto& e = m_edges[ei];
+			const auto vi0 = fvis[fei];
+			const auto vi1 = fvis[p1m3(fei)];
+			const auto pi = (vi0 <= vi1) ? 0 : 1;
+			const array<int, 2> fviS{ std::min(vi0, vi1), std::max(vi0, vi1) };
+			alwaysAssert(e.m_vis == fviS);
+//			alwaysAssert(e.m_fis[pi] == fi); Does not hold for duplicate edges
+			alwaysAssert(e.m_fis[pi] != -1);
+		}
+	}
+
+	const auto& vertexEdgeIndicesRanges = m_vertexEdgeIndicesRanges;
+	const auto& allVertexEdgeLists = m_allVertexEdgeLists;
+	for (int vi = 0; vi < V; ++vi)
+	{
+		const auto veir = vertexEdgeIndicesRanges[vi];
+		const auto first = veir.m_first;
+		alwaysAssert(0 <= first && first < allVertexEdgeLists.size());
+		const auto num = veir.m_num;
+		const auto end = first + num;
+		alwaysAssert(0 <= end&& end <= allVertexEdgeLists.size());
+		alwaysAssert(0 <= first);
+		if (0 < vi)
+			alwaysAssert(vertexEdgeIndicesRanges[vi - 1].m_first + vertexEdgeIndicesRanges[vi - 1].m_num == first);
+
+		if (vi<V-1)
+			alwaysAssert(end == vertexEdgeIndicesRanges[vi + 1].m_first);
+		for (int vei = first; vei < end; ++vei)
+		{
+			const auto ei = allVertexEdgeLists[vei];
+			alwaysAssert(0 <= ei && ei < E);
+			const auto& e = edges[ei];
+			alwaysAssert(e.m_vis[0] == vi || e.m_vis[1] == vi);
+			for (int j = vei + 1; j < end; ++j)
+				alwaysAssert(allVertexEdgeLists[j] != ei);//unique edge
+		}
+	}
+
+	for (int ei = 0; ei < E; ++ei)
+	{
+		const auto& e = edges[ei];
+		assert(e.m_vis[0] < e.m_vis[1]);
+		for (int evi = 0; evi < 2; ++evi)
+		{
+			const auto vi = e.m_vis[evi];
+			alwaysAssert(0 <= vi && vi < V);
+			const auto veir = vertexEdgeIndicesRanges[vi];
+			const auto end = veir.m_first + veir.m_num;
+			alwaysAssert(rangeContains(allVertexEdgeLists, veir.m_first, end, ei));
+		}
+		for (int efi = 0; efi < 2; ++efi)
+		{
+			const auto fi = e.m_fis[efi];
+			if (fi != -1)
+			{
+				assert(0 <= fi && fi < F);
+				const auto& feis = m_faceEdges[fi].m_feis;
+				assert(contains(feis, ei));
+			}
+		}
+	}
+}
